e35d2c37e06240012dcce7ef03f533fc6a05ec9f
maggy <perezmag5137@gmail.com>
2024-03-16 22:09:46 -0400

    Removed Button and added new function where if an amount is placed, tip and total is calculated.
    The slider needs to be worked on to be able to adjust the tip percentage that also changes Tip amount and Total.

--- src/main/java/com/example/javafxtipcalculator/TipCalculatorController.java
+++ src/main/java/com/example/javafxtipcalculator/TipCalculatorController.java
@@ -40,23 +40,31 @@ public class TipCalculatorController {
     // calculates and displays the tip and total amounts
     @FXML
     private void calculateButtonPressed(ActionEvent event) {
-        try {
-            BigDecimal amount = new BigDecimal(amountTextField.getText());
-            BigDecimal tip = amount.multiply(tipPercentage);
-            BigDecimal total = amount.add(tip);
 
-            tipTextField.setText(currency.format(tip));
-            totalTextField.setText(currency.format(total));
-        }
-        catch (NumberFormatException ex) {
-            amountTextField.setText("Enter amount");
-            amountTextField.selectAll();
-            amountTextField.requestFocus();
-        }
     }
 
     // called by FXMLLoader to initialize the controller
     public void initialize() {
+        amountTextField.textProperty().addListener(
+                new ChangeListener<String>() {
+                    @Override
+                    public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+
+                        try {
+                            BigDecimal amount = new BigDecimal(amountTextField.getText());
+                            BigDecimal tip = amount.multiply(tipPercentage);
+                            BigDecimal total = amount.add(tip);
+
+                            tipTextField.setText(currency.format(tip));
+                            totalTextField.setText(currency.format(total));
+                        }
+                        catch (NumberFormatException ex) {
+                            System.out.println("Enter Amount");
+                        }
+                    }
+                }
+        );
+
         // 0-4 rounds down, 5-9 rounds up
         currency.setRoundingMode(RoundingMode.HALF_UP);
 
@@ -69,9 +77,9 @@ public class TipCalculatorController {
                         tipPercentage =
                                 BigDecimal.valueOf(newValue.intValue() / 100.0);
                         tipPercentageLabel.setText(percent.format(tipPercentage));
-                        //no button branch
+
                     }
-                }
+                }  // end of ChangeListner<Number>
         );
     }
 }
